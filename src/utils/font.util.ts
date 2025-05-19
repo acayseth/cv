f792be20472a9f67d1978c62669bacd5946a925e
acayseth <gorbulea.s@gmail.com>
2025-05-19 20:09:46 +0300

    first commit

---
+++ src/utils/font.util.ts
@@ -0,0 +1,11 @@
+import { Roboto, Roboto_Mono } from "next/font/google";
+
+export const fontSans = Roboto({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+});
+
+export const fontMono = Roboto_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
